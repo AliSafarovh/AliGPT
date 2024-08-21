b8fd76e03a67824bdd0d3a1e0226150fab9e5e17
unknown <alisafarovh313@gmail.com>
2024-08-21 15:38:09 +0400

    sad

--- ReCapProject/MyCarProject/Business/obj/Release/net8.0/Business.AssemblyInfo.cs
+++ ReCapProject/MyCarProject/Business/obj/Release/net8.0/Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e3847bd3c18c187579e714c00222e709082ac0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f0f2f376699409878b6d3c5eacabc258d06aded")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ReCapProject/MyCarProject/Entities/obj/Release/net8.0/Entities.AssemblyInfo.cs
+++ ReCapProject/MyCarProject/Entities/obj/Release/net8.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e3847bd3c18c187579e714c00222e709082ac0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f0f2f376699409878b6d3c5eacabc258d06aded")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
