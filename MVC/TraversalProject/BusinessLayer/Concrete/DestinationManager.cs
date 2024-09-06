2981f18f789da347240d6861dd208e7434c38e23
unknown <alisafarovh313@gmail.com>
2024-09-06 14:15:44 +0400

    new codes

---
+++ MVC/TraversalProject/BusinessLayer/Concrete/DestinationManager.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class DestinationManager : IDestinationService
+    {
+
+        IDestinationDal _destinationalDal;
+            public DestinationManager(IDestinationDal destinationDal)
+        {
+            _destinationalDal = destinationDal;
+        }
+        public void Add(Destination entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Destination entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Destination GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Destination> GetList()
+        {
+            return _destinationalDal.GetList();
+        }
+
+        public void Update(Destination entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
